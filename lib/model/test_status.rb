6e3c8a178c4dafc8df7594cbcbd6cc3873194633
aureliano <aureliano.franca@hotmail.com>
2014-07-03 13:38:48 -0300

    Cria modelo para status de teste.

---
+++ lib/model/test_status.rb
@@ -0,0 +1,31 @@
+module Model
+
+  class TestStatus
+  
+    def initialize(opt = {})
+      if block_given?
+        yield self
+      else
+        _load_properties opt
+      end
+    end
+    
+    attr_accessor :test_file_status, :test_text_status
+    
+    def success?
+      ((@test_file_status == true) && (@test_text_status == true))
+    end
+    
+    def error?
+      !success?
+    end
+    
+    private
+    def _load_properties(opt)
+      @test_file_status = opt[:test_file_status]
+      @test_text_status = opt[:test_text_status]
+    end
+  
+  end
+
+end
---
+++ test/unit/model/test_test_status.rb
@@ -0,0 +1,58 @@
+require 'test/unit'
+require File.expand_path '../../../../lib/model/test_status.rb', __FILE__
+
+class TestTestStatus < Test::Unit::TestCase
+
+  include Model
+  
+  def test_initialization
+    _hash_initialization
+    _block_initialization
+  end
+  
+  def test_success
+    status = TestStatus.new :test_file_status => true, :test_text_status => true
+    assert status.success?
+    
+    status.test_file_status = false
+    assert status.success? == false
+    
+    status.test_text_status = false
+    assert status.success? == false
+    
+    status.test_file_status = true
+    assert status.success? == false
+  end
+  
+  def test_error
+    status = TestStatus.new :test_file_status => false, :test_text_status => false
+    assert status.error?
+    
+    status.test_file_status = true
+    assert status.error?
+    
+    status.test_text_status = true
+    assert status.error? == false
+  end
+  
+  private
+  def _hash_initialization
+    status = TestStatus.new({
+      :test_file_status => true, :test_text_status => false
+    })
+    
+    assert status.test_file_status == true
+    assert status.test_text_status == false
+  end
+  
+  def _block_initialization
+    status = TestStatus.new do |s|
+      s.test_file_status = true
+      s.test_text_status = false
+    end
+    
+    assert status.test_file_status == true
+    assert status.test_text_status == false
+  end
+
+end
