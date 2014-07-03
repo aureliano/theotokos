410c54232fe8c747c438b6d9879ca2b5c1588b01
aureliano <aureliano.franca@hotmail.com>
2014-07-03 11:07:48 -0300

    Cria classe de modelo de teste.

--- lib/model/test_suite.rb
+++ lib/model/test_suite.rb
@@ -1,13 +1,30 @@
 module Model
-
+      
   class TestSuite
   
     def initialize(opt = {})
-      @source = opt[:source]
-      @wsdl = opt[:wsdl]
-      @service = opt[:service]
+      if block_given?
+        yield self
+      else
+        _load_properties opt
+      end
+    end
+    
+    attr_accessor :source, :wsdl, :service, :tags, :tests
+    
+    def validate_model!
+      raise Exception, 'WS test model must be provided.' if @source.nil? || @source.empty?
+      raise Exception, "WSDL's URL must be provided." if @wsdl.nil? || @wsdl.empty?
+      raise Exception, 'Service name (service to be tested) must be provided.' if @service.nil? || @service.empty?
+    end
+    
+    private
+    def _load_properties(opt)
+      self.source = opt[:source]
+      self.wsdl = opt[:wsdl]
+      self.service = opt[:service]
       
-      @tags = if opt[:tags].instance_of? String
+      self.tags = if opt[:tags].instance_of? String
         opt[:tags].split(/[,\s]/).select {|t| !t.empty? }
       elsif opt[:tags].instance_of? Array
         opt[:tags]
@@ -15,8 +32,6 @@ module Model
         []
       end
     end
-    
-    attr_accessor :source, :wsdl, :service, :tags, :tests
   
   end
   
--- test/unit/model/test_test_suite.rb
+++ test/unit/model/test_test_suite.rb
@@ -6,6 +6,32 @@ class TestTestSuite < Test::Unit::TestCase
   include Model
   
   def test_initialization
+    _hash_initialization
+    _block_initialization
+  end
+  
+  def test_validation
+    props = {
+      :source => '/path/to/test/case/model/file', :wsdl => 'http://path/to/wsdl?wsdl',
+      :service => 'test_service', :tags => ['dev', 'test', 'production']
+    }
+    
+    assert_nothing_raised { TestSuite.new(props).validate_model! }
+
+    props[:source] = nil
+    assert_raise(Exception, 'WS test model must be provided.') { TestSuite.new(props).validate_model! }
+    
+    props[:source] = '/path/to/test/case/model/file'
+    props[:wsdl] = nil
+    assert_raise(Exception, "WSDL's URL must be provided.") { TestSuite.new(props).validate_model! }
+    
+    props[:wsdl] = 'http://path/to/wsdl?wsdl'
+    props[:service] = nil
+    assert_raise(Exception, 'Service name (service to be tested) must be provided.') { TestSuite.new(props).validate_model! }
+  end
+  
+  private
+  def _hash_initialization
     props = {
       :source => '/path/to/test/case/model/file', :wsdl => 'http://path/to/wsdl?wsdl',
       :service => 'test_service', :tags => ['dev', 'test', 'production']
@@ -23,6 +49,21 @@ class TestTestSuite < Test::Unit::TestCase
     
     props[:tags] = nil
     assert_equal TestSuite.new(props).tags, []
+  end
+  
+  def _block_initialization
+    suite = TestSuite.new do |suite|
+      suite.source = '/path/to/test/case/model/file'
+      suite.wsdl = 'http://path/to/wsdl?wsdl'
+      suite.service = 'test_service'
+      suite.tags = ['dev', 'test', 'production']
+    end
+        
+    assert_equal suite.source, '/path/to/test/case/model/file'
+    assert_equal suite.wsdl, 'http://path/to/wsdl?wsdl'
+    assert_equal suite.service, 'test_service'
+    assert_equal suite.tags, ['dev', 'test', 'production']
+    assert_nil suite.tests
   end
 
 end
