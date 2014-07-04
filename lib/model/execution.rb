4c8e4d4baf8b60f45cca35f0a1fef8bd869887aa
aureliano <aureliano.franca@hotmail.com>
2014-07-04 09:38:23 -0300

    Cria modelo de execução.

---
+++ lib/model/execution.rb
@@ -0,0 +1,14 @@
+module Model
+
+  class Execution
+  
+    def initialize
+      @report_formats = [:console]
+      yield self if block_given?
+    end
+    
+    attr_accessor :test_files, :test_index, :report_formats, :tags
+  
+  end
+
+end
---
+++ test/unit/model/test_execution.rb
@@ -0,0 +1,25 @@
+require 'test/unit'
+require File.expand_path '../../../../lib/model/execution.rb', __FILE__
+
+class TestExecution < Test::Unit::TestCase
+
+  include Model
+  
+  def test_initialization
+    exec = Execution.new
+    assert_equal exec.report_formats, [:console]
+    assert_nil exec.test_files
+    assert_nil exec.test_index
+    assert_nil exec.tags
+    
+    exec.test_files = ['/path/to/file.yml']
+    assert_equal exec.test_files, ['/path/to/file.yml']
+    
+    exec.test_index = 1
+    assert_equal exec.test_index, 1
+    
+    exec.tags = ['dev', 'test']
+    assert_equal exec.tags, ['dev', 'test']
+  end
+  
+end
