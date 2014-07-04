cf4165075407cfcb403a18f1f7787038248b7fd8
aureliano <aureliano.franca@hotmail.com>
2014-07-04 11:47:47 -0300

    Prepara projeto para ser executado como um aplicativo.

---
+++ bin/requires.rb
@@ -0,0 +1,15 @@
+require File.expand_path  '../../lib/config/app_config_params.rb', __FILE__
+require File.expand_path '../../lib/engine/execution_initializer.rb', __FILE__
+require File.expand_path '../../lib/model/execution.rb', __FILE__
+require File.expand_path '../../lib/model/test_app_result.rb', __FILE__
+require File.expand_path '../../lib/model/test.rb', __FILE__
+require File.expand_path '../../lib/model/test_result.rb', __FILE__
+require File.expand_path '../../lib/model/test_status.rb', __FILE__
+require File.expand_path '../../lib/model/test_suite.rb', __FILE__
+require File.expand_path '../../lib/model/test_suite_result.rb', __FILE__
+require File.expand_path '../../lib/model/text_operator.rb', __FILE__
+require File.expand_path '../../lib/model/ws_config.rb', __FILE__
+
+include Model
+include Configuration
+include Engine
