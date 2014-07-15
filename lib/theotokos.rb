256ca02c74a3cad7046b03149b2a9f8889283f59
aureliano <aureliano.franca@hotmail.com>
2014-07-15 15:12:04 -0300

    Create main class.

---
+++ lib/theotokos.rb
@@ -0,0 +1,58 @@
+require File.expand_path '../requires.rb', __FILE__
+
+module Theotokos
+
+  class << self
+  
+    def initialize
+      yield self if block_given?
+      AppLogger.add_stdout_logger
+    end
+    
+    attr_accessor :execution, :ws_config
+    attr_reader :test_app_result
+    
+    def execute
+      AppConfigParams.load_app_config_params
+      
+      logger.info 'Initialize WS test application...'
+      logger.info "Mapping test models in #{((@execution.execution_path) ? @execution.execution_path : ENV['ws.test.models.path'])}"
+      
+      command.test_files = ExecutionInitializer.load_test_models execution.execution_path
+      logger.debug "Got test models: #{execution.test_files.join(', ')}"
+      
+      _execute
+    end
+    
+    def save_reports
+      execution.report_formats.reject {|e| e == :console }.each do |format|
+        case format
+          when :html then Reporter.create_reporter(format).print(:app_result => @test_app_result, :ws_config => @ws_config, :tags => @execution.tags)
+          when :json then Reporter.create_reporter(format).print @test_app_result
+      end
+    end
+    
+    def logger
+      return @log if @log
+      @log = AppLogger.create_logger (Theotokos = Class.new)
+      @log
+    end
+    
+    private
+    def _execute
+      logger.info 'Initialize executors'
+      init_time = Time.now
+      
+      main_executor = ExecutionInitializer.new do |e|
+        e.command = @execution
+        e.ws_config = @ws_config
+      end
+      
+      main_executor.init_executors
+      @test_app_result = main_executor.test_app_result
+      @test_app_result.date_report = init_time
+    end
+    
+  end
+
+end
