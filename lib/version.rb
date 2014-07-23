34330ff88340c8d87abf459e678354996eb8393e
aureliano <aureliano.franca@hotmail.com>
2014-07-23 20:36:03 -0300

    Create a new release.

--- lib/version.rb
+++ lib/version.rb
@@ -2,7 +2,7 @@ module Theotokos
   VERSION_NUMBERS = [
     VERSION_MAJOR = 0,
     VERSION_MINOR = 1,
-    VERSION_BUILD = 0,
+    VERSION_BUILD = 1,
   ]
   VERSION = VERSION_NUMBERS.join(".")
 end
