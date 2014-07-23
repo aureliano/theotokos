4fcbe2d4eab65024e8c81a79e3b5a6145708b1fd
aureliano <aureliano.franca@hotmail.com>
2014-07-23 20:15:02 -0300

    Creates first release.

--- lib/version.rb
+++ lib/version.rb
@@ -1,8 +1,8 @@
 module Theotokos
   VERSION_NUMBERS = [
     VERSION_MAJOR = 0,
-    VERSION_MINOR = 0,
-    VERSION_BUILD = 1,
+    VERSION_MINOR = 1,
+    VERSION_BUILD = 0,
   ]
   VERSION = VERSION_NUMBERS.join(".")
 end
